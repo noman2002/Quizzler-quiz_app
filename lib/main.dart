904d6b60b7c929d600de7b06e7f403d1a99197af
noman2002 <nomn2002@gmail.com>
2021-06-11 01:06:15 +0530

    added questions

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
-  int questionNumber=0;
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[0],
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,7 +68,11 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  questionNumber++;
+                });
+              },
             ),
           ),
         ),
