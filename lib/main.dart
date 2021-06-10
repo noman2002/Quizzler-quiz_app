4b46d3524b2ca379e7b772452be12b60cd093977
noman2002 <nomn2002@gmail.com>
2021-06-11 00:41:54 +0530

    added questions

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
   List<String> questions = [
 'You can lead a cow down stairs but not up stairs.',
-'Approximately one quarter of human bones are in the feet.'
+'Approximately one quarter of human bones are in the feet.',
+'A slug\'s blood is green.',
 
   ];
 
