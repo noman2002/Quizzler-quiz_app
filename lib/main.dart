3d5dcf70b9d6866efbb2de660f8d2774edbd1536
noman2002 <nomn2002@gmail.com>
2021-06-13 17:30:37 +0530

    added quiz brain

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+
 
 void main() => runApp(Quizzler());
 
@@ -30,14 +30,7 @@ class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
  
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-      q: 'Approximately one quarter of human bones are in the feet.',
-      a: true,
-    ),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
+ 
 
   int questionNumber = 0;
 
