4e6af0fde126e1d5ecde19e96cf9d32769bf675f
noman2002 <nomn2002@gmail.com>
2021-06-13 16:57:29 +0530

    added question class

--- lib/main.dart
+++ lib/main.dart
@@ -39,9 +39,14 @@ class _QuizPageState extends State<QuizPage> {
     true,
   ];
 
-  Question q1 = Question(q:'You can lead a cow down stairs but not up stairs.',a:false);
-  Question q2 = Question(q: 'Approximately one quarter of human bones are in the feet.',a:true);
-  Question q3 = Question(q:'A slug\'s blood is green.',a:true);
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
 
   int questionNumber = 0;
 
