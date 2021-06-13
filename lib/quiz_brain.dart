1ee33ae604be5981ee311a4b1944a5169633bd1e
noman2002 <nomn2002@gmail.com>
2021-06-13 18:02:58 +0530

    added quiz brain

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,10 +1,12 @@
 import 'package:quizzler/question.dart';
 
-class QuizBrain{
-   List<Question> questionBank = [
-   Question(q: 'Some cats are actually allergic to humans', a: true),
+class QuizBrain {
+  List<Question> questionBank = [
+    Question(q: 'Some cats are actually allergic to humans', a: true),
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
     Question(q: 'A slug\'s blood is green.', a: true),
     Question(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
     Question(q: 'It is illegal to pee in the Ocean in Portugal.', a: true),
@@ -27,6 +29,5 @@ class QuizBrain{
     Question(
         q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         a: true),
-
   ];
 }
